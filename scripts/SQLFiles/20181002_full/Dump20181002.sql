ea35a8b6d1a56cd6e2725587dff3aadad9c06f82
kumaravel <kumar.okm1995@gmail.com>
2018-10-02 22:42:59 +0530

    dd

--- scripts/SQLFiles/20181002_full/Dump20181002.sql
+++ scripts/SQLFiles/20181002_full/Dump20181002.sql
@@ -1,4 +1,8 @@
 CREATE DATABASE  IF NOT EXISTS `gymmanagement` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+
+-- test 
+
 USE `gymmanagement`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
